83a57ed5e6e8e1efdb2ec0d320f4880fb2a7616d
Troy Goode <troygoode@gmail.com>
2025-02-21 21:59:22 -0800

    attempting to fix weird github actions failure (again)

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,22 @@
 import type { NextConfig } from "next";
-import path from "path";
 
-const __dirname = new URL(".", import.meta.url).pathname;
+// import path from "path";
+// const __dirname = new URL(".", import.meta.url).pathname;
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+
+  // context: weird failure in Github action during deploy
+  // https://github.com/vercel/next.js/discussions/27666
   // param defined as "any" in Next's NextJsWebpackConfig
-  webpack: (config: any) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-    return config;
-  },
+  // webpack: (config: any) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     "@": path.resolve(__dirname, "./"),
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
