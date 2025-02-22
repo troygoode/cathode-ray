09099851ed7c25d2ea8a14121cf54045487b151a
Troy Goode <troygoode@gmail.com>
2025-02-21 21:53:35 -0800

    attempting to fix weird github actions failure

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
+
+const __dirname = new URL(".", import.meta.url).pathname;
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  // param defined as "any" in Next's NextJsWebpackConfig
+  webpack: (config: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
